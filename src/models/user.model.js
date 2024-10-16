630ac2625569d3c5253553d914ec0999580376bf
Naphees-7667 <naphees6200@gmail.com>
2024-10-16 17:48:34 +0530

    User model complited

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
         trim:true
     },
 
-    fullname:{
+    fullName:{
         type:String,
         required:true,
         trim:true,
@@ -60,6 +60,35 @@ userSchema.pre("save",async function(next){
 
 userSchema.methods.matchPassword = async function(enetredPassword){
     return await bcrypt.compare(enetredPassword,this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 const User = mongoose.model("User",userSchema);
