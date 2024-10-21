9b57972bf6eb49302b8b18888e6d61df5ea9601e
Naphees-7667 <naphees6200@gmail.com>
2024-10-21 11:32:57 +0530

    watch history funtionality added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -350,6 +350,8 @@ const getWatchHistory = asyncHandler(async (req, res) => {
             }
         }
     ])
+
+    return res.status(200).json(new ApiResponse(200, user[0], "Watch History fetched successfully"));
 })
 
 export {
